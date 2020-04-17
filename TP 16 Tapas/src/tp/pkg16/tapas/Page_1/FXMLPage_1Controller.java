549a473bf0dce308c25befa7fa0369f036ebecd5
Denise <denise.hugo1805@gmail.com>
2020-04-17 13:09:47 +0200

    test switch réussi

--- TP 16 Tapas/src/tp/pkg16/tapas/Page_1/FXMLPage_1Controller.java
+++ TP 16 Tapas/src/tp/pkg16/tapas/Page_1/FXMLPage_1Controller.java
@@ -7,27 +7,18 @@ package tp.pkg16.tapas.Page_1;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 
 /**
+ * FXML Controller class
  *
  * @author denis
  */
 public class FXMLPage_1Controller implements Initializable {
-    
-    @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        Button source = (Button)event.getSource();
-        System.out.println(source.getText());
-    }
-    
+
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
