6dded5667aba8b471daf4ec04afdd8c98b6ef297
Denise <denise.hugo1805@gmail.com>
2020-05-13 17:01:18 +0200

    2éme page

--- TP 16 Tapas/src/Class/Session.java
+++ TP 16 Tapas/src/Class/Session.java
@@ -11,6 +11,15 @@ package Class;
  */
 public class Session {
     public static int _page = 0;
+    public static int _nbpersonne = 0;
+
+    public static int getNbpersonne() {
+        return _nbpersonne;
+    }
+
+    public static void setNbpersonne(int _nbpersonne) {
+        Session._nbpersonne = _nbpersonne;
+    }
 
     public static int getPage() {
         return _page;
--- TP 16 Tapas/src/nombre/FXMLPage_1Controller.java
+++ TP 16 Tapas/src/nombre/FXMLPage_1Controller.java
@@ -5,9 +5,14 @@
  */
 package nombre;
 
+import Class.Session;
+import Class.Switcher;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 /**
  * FXML Controller class
@@ -23,5 +28,14 @@ public class FXMLPage_1Controller implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+    @FXML
+        private void addNumber(ActionEvent event) 
+        {
+            Button source = (Button)event.getSource();
+            Session.setNbpersonne(Integer.parseInt(source.getText()));
+            System.out.println(Session.getNbpersonne());
+            Switcher.changePage("tapas");
+        }
     
 }
